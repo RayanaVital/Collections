4134d1748ee0504739b6c9096b41b493bd4a6175
RayanaVital <96187743+RayanaVital@users.noreply.github.com>
2022-02-01 16:54:26 -0300

    Update TestCollections.java

--- src/br/com/collections/TestCollections.java
+++ src/br/com/collections/TestCollections.java
@@ -1,7 +1,11 @@
 package br.com.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class TestCollections {
 
@@ -9,38 +13,78 @@ public class TestCollections {
 		
 		//a interface list armazena elementos ordenados pela inserçao que podem ser repetidos, armazena elemantos num array interno que pode diminuir ou aumentar
 
-		ArrayList carrinho = new ArrayList (); // declaramos e intanciamos
-		Double valor = 100.55;
-		int valor2 = 1;
-		int valor3;
-		carrinho.add(valor); //adicionamos valor ao array list carrinho 
-		carrinho.add ("uva");
-        carrinho.add (valor2);
+//		ArrayList carrinho = new ArrayList (); // declaramos e intanciamos
+//		Double valor = 100.55;
+//		int valor2 = 1;
+//		int valor3;
+//		carrinho.add(valor); //adicionamos valor ao array list carrinho 
+//		carrinho.add ("uva");
+//        carrinho.add (valor2);
 		
-		System.out.println(carrinho.size());
-		System.out.println(carrinho.get(1)); // da o valor do array posicionado em indice 1
+//		System.out.println(carrinho.size());
+//		System.out.println(carrinho.get(1)); // da o valor do array posicionado em indice 1
 		
 //		valor3 = carrinho.get(2);//não compila, precisaria de um cast
 		
-		valor3 = (int) carrinho.get(2);
+//		valor3 = (int) carrinho.get(2); //cast 
 		
-		System.out.println(valor2);
+//		System.out.println(valor2);/*
 		
 		
 		//Interface List e o uso de generics 
 		//Generics não permite o uso de primitivos
 		
-		/*List<String> carrinho = new ArrayList<String>(); //neste array list so podemos ter itens de string, utilizando generics 
+//		List<String> carrinho = new ArrayList<String>(); //neste array list so podemos ter itens de string, utilizando generics 
 		
-		System.out.println(carrinho.isEmpty()); //true pois o array foi instanciado porem não havia intens 
+//		System.out.println(carrinho.isEmpty()); //true pois o array foi instanciado porem não havia intens 
 		
-		carrinho.add("Maça");
-		carrinho.add("Morango");
-		carrinho.add("Maça");
-		carrinho.set(1, "Pera");//altera o conteudo de determinado indice no array
+//		carrinho.add("Maça");
+//		carrinho.add("Morango");
+//		carrinho.add("Maça");
+//		carrinho.set(1, "Pera");//altera o conteudo de determinado indice no array
 		
-		System.out.println(carrinho.isEmpty()); // se o carrinho esta vazio 
-		System.out.println(carrinho.size()); //determina o tamanho do array */
+//		System.out.println(carrinho.isEmpty()); // se o carrinho esta vazio 
+//		System.out.println(carrinho.size()); //determina o tamanho do array
+		
+//		System.out.println(carrinho.contains("Maça")); //retorna se a palavra maça esta em alguns dos itens do nosso array
+//		System.out.println(carrinho.indexOf("Maça")); //retorna a posição que a palavra esta
+//		System.out.println(carrinho.get(carrinho.indexOf("Maça"))); //busca a palavra atraves a partir do posição retornada no index of
+//		System.out.println(carrinho.lastIndexOf("Maça")); //ultima ocorrencia dentro do array list 
+//		
+//		carrinho.remove(carrinho.indexOf("Maça"));
+//		System.out.println(carrinho.get(0));
+//		
+//		carrinho.clear();
+//		System.out.println(carrinho.isEmpty());
+		
+//		Set<String> cesta = new HashSet<String>(); //variavel do tipo set chamado cesta 
+//		
+//		System.out.println(cesta.isEmpty()); //se esta vazio 
+//		cesta.add("Maça");
+//		cesta.add("maça");
+//		cesta.add("Maça");
+//		System.out.println(cesta.isEmpty()); //cesta recebeu adição de 3 itens 		
+//		System.out.println(cesta.size()); //size é 2 porque a classe hash set não permite elementos duplicados 
+//		System.out.println(cesta);
+		
+		//Interface map muito util quando precisamos recuperar um valor atraves de sua chave
+		
+		Map<String, String> caixa = new HashMap<String, String>();
+		caixa.put("M225", "Fernando");
+		caixa.put("M226", "Maria");
+		caixa.put("M227", "Soraya");
+		
+		System.out.println(caixa.isEmpty());
+		System.out.println(caixa.size());
+		System.out.println(caixa.containsKey("M225"));
+		System.out.println(caixa.containsValue("Fernando"));
+		System.out.println(caixa);
+		
+		
+		
+		
+		
+		
 		
 		
 		
